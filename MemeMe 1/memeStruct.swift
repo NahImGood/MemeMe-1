fd0e7a55f7d2a34027f8c7a875d86fbb6b9b0cbd
NahImGood <warnere225@gmail.com>
2019-01-30 11:42:37 -0800

    Passed Submission level 1, Marks Start of Part 2. Nothing has been done to start part 2.

---
+++ MemeMe 1/memeStruct.swift
@@ -0,0 +1,19 @@
+//
+//  memeStruct.swift
+//  MemeMe 1
+//
+//  Created by Admin on 1/18/19.
+//  Copyright © 2019 EGW. All rights reserved.
+//
+
+import UIKit
+
+struct Meme {
+    
+    var topTextField: String?
+    var bottomTextField: String?
+    var originalImage: UIImage?
+    var memeImage: UIImage?
+    
+    
+}
